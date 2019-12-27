43b0ebc2d5cae61fe27c0af20139d260c00e1a3c
Henrique Teixeira <henrique.teixeira@evosystems.com.br>
2019-12-27 15:08:38 -0300

    confirm activity

---
+++ app/src/main/java/com/example/bank/ui/transfer/ConfirmTransferActivity.java
@@ -0,0 +1,16 @@
+package com.example.bank.ui.transfer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bank.R;
+
+public class ConfirmTransferActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_transfer);
+    }
+}
